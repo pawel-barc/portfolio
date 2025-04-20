d2a9638ed694b0abb73d099ed1bbcdf30ea806b4
pawel-barc <pawel.barc@laplateforme.io>
2025-04-20 20:08:06 +0200

    Contact Form created, FormSubmit implemented. Anchor navigation successfull terminated

---
+++ script.js
@@ -0,0 +1,41 @@
+//Carrousel
+const slides = document.querySelectorAll(".slide");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+let currentIndex = 0;
+
+function showSlide(index) {
+  const totalSlides = slides.length;
+  if (index >= totalSlides) currentIndex = 0;
+  else if (index < 0) currentIndex = totalSlides - 1;
+  else currentIndex = index;
+
+  const offset = -currentIndex * 100 + "%";
+  document.querySelector(
+    ".carousel-container"
+  ).style.transform = `translateX(${offset})`;
+}
+
+prevBtn.addEventListener("click", () => showSlide(currentIndex - 1));
+nextBtn.addEventListener("click", () => showSlide(currentIndex + 1));
+
+setInterval(() => showSlide(currentIndex + 1), 50000);
+
+// Formulaire
+window.onload = () => {
+  const hash = window.location.hash;
+  if (hash === "#thanks") {
+    const successMessage = document.getElementById("thanks");
+    const form = document.getElementById("contact-form");
+    if (form && successMessage) {
+      form.style.display = "none";
+      successMessage.style.display = "block";
+      successMessage.scrollIntoView({ behavior: "smooth" });
+      setTimeout(() => {
+        form.style.display = "flex";
+        successMessage.style.display = "none";
+        history.replaceState(null, null, "");
+      }, 3000);
+    }
+  }
+};
